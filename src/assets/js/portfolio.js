95f81b0908cf8a38b2507d3358adbb571f42e9df
chloe iy choi <ioiuioiciy88@gmail.com>
2023-08-16 15:30:25 +0900

    포트폴리오 완료

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -1,3 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export function portfolio() {
-  console.log(portfolio)
+    gsap.registerPlugin(ScrollTrigger);
+    const wholeSection = gsap.utils.toArray(".portfolio__item");
+
+    gsap.to(wholeSection, {
+        xPercent: -120 * (wholeSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#portfolio",
+            start: "top 56px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
+    });
 }
